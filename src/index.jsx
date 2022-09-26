5772bfd920fe85e5acf1d296970c44ef9f980e55
Adam Polk <adampolk91@gmail.com>
2022-09-26 11:11:49 -0500

    builds basic repo structure for MVP

---
+++ src/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+
+  );
+}
+
+// eslint-disable-next-line no-undef
+ReactDOM.render(<App />, document.getElementById('root'));
