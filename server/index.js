21706255dd65f107fd8e9987e215a86ab2a6bdb8
Adam Polk <adampolk91@gmail.com>
2022-09-27 10:51:04 -0500

    adds express routing for future API call and completes modal skeleton

--- server/index.js
+++ server/index.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/country/:country', (req, res) => {
+  console.log(req.body);
+  res.send('nice!');
+});
 
 app.listen(3001);
 console.log('Listening on port 3001');
--- src/components/map.jsx
+++ src/components/map.jsx
@@ -1,65 +1,97 @@
 /* eslint-disable import/extensions */
 import React from 'react';
+import axios from 'axios';
 import { ComposableMap, Geographies, Geography, ZoomableGroup } from 'react-simple-maps';
-import CountryModal from './countryModal.jsx';
+import { Modal, Button, Typography, Box } from '@mui/material';
 
-class Map extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      geoURL: 'https://raw.githubusercontent.com/deldersveld/topojson/master/world-countries-sans-antarctica.json',
-      country: false,
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
+const styleModal = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
-  handleClick(e) {
-    const countryName = e.target.getAttribute('name');
-    this.setState({
-      country: countryName,
-    });
-  }
+const styleMap = {
+  default: {
+    fill: '#EEE',
+    outline: 'none',
+  },
+  hover: {
+    fill: 'lightblue',
+    outline: 'none',
+  },
+  pressed: {
+    fill: 'green',
+    outline: 'none',
+  },
+};
 
-  render() {
-    const { geoURL, country } = this.state;
-    return (
-      <div id="world-map">
-        <div id="country-modal">
-          <CountryModal />
-        </div>
-        <h1 id="country-name">{country || '___________'}</h1>
-        <ComposableMap id="map-canvas">
-          <ZoomableGroup center={[0, 0]} zoom={9}>
-            <Geographies geography={geoURL}>
-              {({ geographies }) => geographies.map((geo) => (
-                <Geography
-                  key={geo.rsmKey}
-                  stroke="grey"
-                  geography={geo}
-                  name={geo.properties.name}
-                  onClick={this.handleClick}
-                  style={{
-                    default: {
-                      fill: '#EEE',
-                      outline: 'none',
-                    },
-                    hover: {
-                      fill: 'lightblue',
-                      outline: 'none',
-                    },
-                    pressed: {
-                      fill: 'green', // modal popup?
-                      outline: 'none',
-                    },
-                  }}
-                />
-              ))}
-            </Geographies>
-          </ZoomableGroup>
-        </ComposableMap>
+const mapURL = 'https://raw.githubusercontent.com/deldersveld/topojson/master/world-countries-sans-antarctica.json';
+
+function Map() {
+  const [geoURL] = React.useState(mapURL);
+  const [country, setCountry] = React.useState(false);
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const fetchCountryData = (countryToFind) => {
+    axios.get(`http://localhost:3001/country/${countryToFind}`)
+      .then((response) => console.log(response))
+      .catch((error) => console.log(error));
+  };
+
+  const handleCountryClick = (e) => {
+    const currentCountry = e.target.getAttribute('name');
+    setCountry(currentCountry);
+    fetchCountryData(currentCountry);
+    handleOpen();
+  };
+
+  return (
+    <div id="world-map">
+      <h1 id="country-name">{country || '___________'}</h1>
+      <ComposableMap id="map-canvas">
+        <ZoomableGroup center={[10, 10]} zoom={1.20}>
+          <Geographies geography={geoURL}>
+            {({ geographies }) => geographies.map((geo) => (
+              <Geography
+                key={geo.rsmKey}
+                stroke="grey"
+                geography={geo}
+                name={geo.properties.name}
+                onClick={handleCountryClick}
+                style={styleMap}
+              />
+            ))}
+          </Geographies>
+        </ZoomableGroup>
+      </ComposableMap>
+      <div>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={styleModal}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Text in a modal
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+            </Typography>
+          </Box>
+        </Modal>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Map;
